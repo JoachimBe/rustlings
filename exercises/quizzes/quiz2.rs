d01ce8f15e95e2c24cdc2d36005a48a3c9332511
unknown <j.bertrand0032@gmail.com>
2026-05-22 13:37:19 +0200

    errors almost done

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,24 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String>{
+        let mut result: Vec<String> = vec![];
+        for task in input{
+            let mut string_to_modify= task.0;
+            let command_execute= task.1;
+            match command_execute {
+                Command::Uppercase=>{result.push(string_to_modify.to_uppercase())},
+                Command::Trim=>{result.push(string_to_modify.trim().to_string())},
+                Command::Append(value)=> {
+                    for _i in 0..value{
+                        string_to_modify.push_str("bar");
+                    }
+                    result.push((string_to_modify).to_string());
+                },
+            }
+        }
+        result
+    }
 }
 
 fn main() {
@@ -38,6 +56,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
